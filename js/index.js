1cda729eb48dffd4b026ff94320417f3a3c9799f
muxtorjonov2005 <muxtorjonoveldiyor@gmail.com>
2024-07-19 10:04:47 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,234 @@
+// 1-masala  
+// let age = +prompt('yosh kiriting')
+// if(age >= 18){
+//     console.log('Siz katta yoshdasiz');
+// }else{
+//     console.log('Siz xali yoshsiz');
+// }
+
+
+// 2-masala
+// let num = +prompt('yosh kiriting')
+// if(num % 2 == 0){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+// 3-masala
+// let num = +prompt('yosh kiriting')
+// if(num > 0){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+// 4-masala
+// let temp = +prompt('yosh kiriting')
+// if(temp >= 20){
+//     console.log('Yengil kiyining');
+// }else{
+//     console.log('qalin qiyining');
+// }
+
+
+// 5-masala
+// let score = +prompt('yosh kiriting')
+// if(score >= 90){
+//     console.log('Alo');
+// }else if(score >= 70){
+//     console.log('Yaxshi');
+// }else if(score >= 50){
+//     console.log('Qoniqarli');
+// }else{
+//     console.log('Yamon');
+// }
+
+
+// 6-masala
+// let ghostName = +prompt('yosh kiriting')
+// if(ghostName === 'Ali'){
+//     console.log('Salom, Ali');
+// }else{
+    // console.log('Salom, mehmon');
+// }
+
+
+// 7-masala
+// let cost = +prompt('yosh kiriting')
+// if(cost >= 100){
+//     console.log('chegirma qollaniladi');
+// }else{
+//     console.log('chegirma yoq');
+// }
+
+
+
+// 1-masala
+// let temp = +prompt('yosh kiriting')
+// temp > 25 ? console.log('Issiq') : console.log('Sovuq');
+
+
+// 2-masala
+// let score = +prompt('yosh kiriting')
+// score >= 50 ? console.log('otti') : console.log('yiqildi');
+
+
+// 3-masala
+// let num = +prompt('yosh kiriting')
+// num > 0? console.log('musabat') : console.log('Manfiy');
+
+
+
+// 1-masala
+// for(let i = 1; i <= 10; i++){
+    // console.log(i);
+// }
+
+
+// 2-masala
+// for(let i = 10; i >= 1; i--){
+    // console.log(i);
+// }
+
+
+// 3-masala
+// for(let i = 2; i <= 100; i += 2){
+    // console.log(i);
+// }
+
+
+// 4-masala
+// for(let i = 1; i <= 100; i += 2){
+    // console.log(i);
+// }
+
+
+// 5-masala
+// let sum = 0
+// for(let i = 1; i <= 10; i++){
+//     sum = sum + i
+//     console.log(sum);
+// }
+
+
+// 6-masala
+// let res = 1
+// for(let i = 1; i <= 10; i++){
+//     res = res * i
+//     console.log(res);
+// }
+
+
+// 7-masala
+let num = +prompt('yosh kiriting')
+let fact = 1
+for(let i = 1; i <= num; i++){
+    fact *= i
+    console.log(fact);
+}
+
+
+// 8-masala
+// for(let i = 1; i <= 50; i++){
+    // console.log(i * i);
+// }
+
+
+// 9-masala
+// for(let i = 1; i <= 20; i++){
+    // console.log(i ** 3);
+// }
+
+
+// 10-masala
+// let sum = 0
+// for(let i = 1; i <= 1000; i++){
+//     sum = sum + i
+//     console.log(sum);
+// }
+
+
+// 11-masala
+// for(let i = 0; i < 5; i++){
+    // console.log('Salom');
+// }
+
+
+// 12-masala
+// for(let i = 1; i <= 100; i++){
+    // console.log(i * 2);
+// }
+
+
+// 13-masala
+// for(let i = 1; i <= 10; i++){
+    // console.log(2 ** i);
+// }
+
+
+// 14-masala
+// let num = +prompt('yosh kiriting')
+// for(let i = 2; i <= 2; i++){
+    // console.log(num ** i);
+// }
+
+
+// 15-masala
+// for(let i = 'a'; i <= 'z'; i++){
+    // console.log(i);
+// }
+
+
+
+// 1-masala
+// let a = +prompt('yosh kiriting')
+// let b = +prompt('yosh kiriting')
+// if(a % 2 == 1 && b % 2 == 1){
+    // console.log(true);
+// }
+
+
+// 2-masala
+// let a = +prompt('yosh kiriting')
+// let b = +prompt('yosh kiriting')
+// let c = +prompt('yosh kiriting')
+// if(a > 0 && b > 0 && c > 0){
+    // console.log(true);
+// }
+
+
+// 3-masala
+// let a = +prompt('yosh kiriting')
+// let b = +prompt('yosh kiriting')
+// let c = +prompt('yosh kiriting')
+// if(a > 0 || b > 0 || c > 0){
+    // console.log(true);
+// }
+
+
+// 4-masala
+// let a = +prompt('yosh kiriting')
+// let b = +prompt('yosh kiriting')
+// let c = +prompt('yosh kiriting')
+// if(a > 0 && b < 0 && c < 0 || a < 0 && b > 0 && c < 0 || a < 0 && b < 0 && c > 0){
+    // console.log(true);
+// }
+
+
+// 5-masala
+// let a = +prompt('yosh kiriting')
+// let b = +prompt('yosh kiriting')
+// let c = +prompt('yosh kiriting')
+// if(a > 0 && b > 0 && c < 0 || a > 0 && b < 0 && c > 0 || a < 0 && b > 0 && c > 0){
+    // console.log(true);
+// }
+
+
+// 6-masala
+// let num = +prompt('yosh kiriting')
+// if(num % 2 == 0 && num > 9 && num < 100){
+    // console.log(true);
+// }
